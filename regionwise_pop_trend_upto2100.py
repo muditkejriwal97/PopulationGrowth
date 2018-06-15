efa3af65c27329f6a077eaa4b55b3d3fab36415b
Mudit Kejriwal <muditkejriwal97@gmail.com>
2018-06-15 16:50:48 +0530

    added details

--- regionwise_pop_trend_upto2100.py
+++ regionwise_pop_trend_upto2100.py
@@ -2,23 +2,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 
+#Reading File and Parsing
 file = 'Data Population - v5 - 1800 to 2100 World Regions and Countries by Gapminder.xlsx'
 data = pd.ExcelFile(file)
 df1 = data.parse('pivoted_region_data_for_chart')
+
+#Transpose of a dataframe and dropping 
 df1 = df1.transpose()
 df1 = df1.drop('geo')
 df1 = df1.drop('indicator')
 df1 = df1.drop('name')
+
+#Renaming columns
 df1.columns = ['America', 'Europe', 'Africa', 'Asia']
+
+#Plotting Graph
 df1.plot.line()
-li= list(df1.index.values)
 
 plt.suptitle('Region Wise Population (1800 to 2100)', fontsize=20)
 plt.xlabel('Year', fontsize=18)
 plt.ylabel('Population(Billions)', fontsize=18)
+
+#Optimizing ticks on x and y axis
 plt.ticklabel_format(useOffset=False, style='plain')
 plt.xticks(range(1800,2120,20))
-
 plt.grid()
+
 plt.show()
 
--- total_pop_trend_upto2100.py
+++ total_pop_trend_upto2100.py
@@ -2,20 +2,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 
+#Reading File and Parsing
 file = 'Data Population - v5 - 1800 to 2100 World Regions and Countries by Gapminder.xlsx'
 data = pd.ExcelFile(file)
 df1 = data.parse('pivoted_country_data_for_chart')
-#df1 = df1.transpose()
+
+#Dropping
 df1 = df1.drop(["geo", "indicator", "name"], axis=1)
+
+#Finding total population for each year
 df2=df1.sum()
 df2.plot()
+
+#Fromatting axis ticks
 plt.ticklabel_format(useOffset=False, style='plain')
 
 plt.suptitle('Global Population(1800 to 2100)', fontsize=20)
 plt.xlabel('Year', fontsize=18)
 plt.ylabel('Population(Billions)', fontsize=18)
 plt.xticks(range(1800,2120,20))
+
 plt.grid()
 plt.margins(0)
-print(df2)
+
 plt.show()
